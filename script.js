59b7c1f1833563cce0cc34fbcf02659d6d1a6201
JanSitarskiKutno <152687535+JanSitarskiKutno@users.noreply.github.com>
2025-01-07 10:08:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let a = document.getElementById("skill");
+
+function skills(x) {
+
+    switch (x)
+    {
+        case 1:
+            a.innerHTML = "C++ uczę się od samego początku mojej nauki programowania, "
+            break;
+    }
+}
